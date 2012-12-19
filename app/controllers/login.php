5fb08d4b20e7877352792af38ddf9387ea3c092c
Efren Bautista <wog.eabautistajr@gmail.com>
2012-12-19 16:00:20 +0800

    Delete files on the login controller

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -2,17 +2,6 @@
 class Login extends Controller{
     function index() {
         
-        $array = array('lastname', 'email', 'phone');
-        $comma_separated = implode(', :', $array);
-        
-        //$fieldValues = ':' . implode(', :', array_keys($data));
-        
-        echo '___'.$comma_separated; // lastname,email,phone
-
-        // Empty string when using an empty array:
-        //echo implode('hello', array());
-        //echo Hash::create('sha256', 'jesse', HASH_PASSWORD_KEY);
-        //echo Hash::create('sha256', 'test2', HASH_PASSWORD_KEY);    
         if(isset($_POST['login'])){
             $this->form_validate->addValidation("username","req","Hey! thats not fair! tell me your username!");
             $this->form_validate->addValidation("password","req","Will you please tell me your password.");
