0f1f7d0157ee4a92631c91a9148fd66fd9f5dc86
Jose Paulo <jplacsinto@gmail.com>
2013-04-18 13:46:38 +0800

    added db_ei

--- app/views/footers/index_footer.php
+++
@@ -1,5 +0,0 @@
-
-<script type="text/javascript" src="<?php echo JS_PATH;?>jquery.js"></script>
-<script type="text/javascript" src="<?php echo JS_PATH;?>bootstrap.min.js"></script>
-</body>
-</html>
